36f942bd5b90bcc01368a35c5cdc66674dc6ce2d
Balzamova <balzam0401@gmail.com>
2021-03-09 16:13:40 +0300

    add function for repeated

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,25 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  this.repeatTimes = options.repeatTimes !== undefined ? options.repeatTimes : 1; //количество повторений str
+  this.separator = options.separator !== undefined ? options.separator : '+'; //разделитель для str или +
+  this.addition = options.addition !== undefined ? options.addition : ''; //что добавить
+  this.additionRepeatTimes = options.additionRepeatTimes !== undefined ? options.additionRepeatTimes : 1; //сколько раз добавить
+  this.additionSeparator = options.additionSeparator !== undefined ? options.additionSeparator : '|'; //разделитель для добавления или |
+  
+  let result='';
+    
+    for (let i = 0; i < this.repeatTimes; i++) {
+        result = result.concat(str);
+
+        for (let j = 0; j < this.additionRepeatTimes; j++) {
+            result = result.concat(this.addition);      
+            j < (this.additionRepeatTimes-1) ? result = result.concat(additionSeparator) : 0;
+        }
+        
+        i < (this.repeatTimes - 1) ? result = result.concat(this.separator) : 0;
+    }
+
+    return result;
 };
   
